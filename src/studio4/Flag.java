df785b22a96c2f4ac4baacac5a6be8566873395a
jaded <jaded@JadensLaptop.dhcp.wustl.edu>
2022-10-13 14:01:38 -0500

    Studio stuff incase im supposed to commit it

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,16 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+
+		StdDraw.setPenColor(60,1,5);
+		StdDraw.filledRectangle(0.5, 0.75, 0.5, 0.25);
+		StdDraw.setPenColor(252,148,3);
+		StdDraw.filledRectangle(0.5, 0.25, 0.5, 0.25);
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledCircle(0.6, 0.5, 0.3);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledCircle(0.65, 0.6, 0.15);
+		StdDraw.rectangle(0.5, 0.5, 0.5, 0.5);
+	
 	}
 }
